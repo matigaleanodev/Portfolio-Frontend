771ac352927a36a9028f6bc141967bb6e7aa78e3
Matias Ariel Galeano <galeano_matias217@yahoo.com.ar>
2022-08-12 15:04:42 -0300

    validadores en login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { SidebarAnimation } from 'src/app/animations/sidebar.animations';
 import { AuthService } from 'src/app/services/authentication/auth.service';
 
@@ -20,7 +21,8 @@ export class LoginComponent {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private toastr: ToastrService
   ) {
     this.authService.isLoggedIn().subscribe(res => {
     this.onLogin = res;
@@ -28,29 +30,31 @@ export class LoginComponent {
   );
     this.loginForm = this.fb.group({
       username: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required], [Validators.minLength(8)]],
     });
   }
 
-  get username() {
+  get Username() {
     return this.loginForm.get('username');
   }
 
-  get password() {
+  get Password() {
     return this.loginForm.get('password');
   }
 
   login(event: Event) {
     event.preventDefault();
     if (this.loginForm.valid) {
+      this.toastr.success('Iniciando Sesion', 'Login');
       let cred = JSON.stringify(this.loginForm.value);
       this.authService.login(cred).subscribe(() => {
         console.log("session iniciada");
         this.closeModal.nativeElement.click();
         this.router.navigate(['/dashboard']);
+        this.toastr.success('Sesion Iniciada', 'Login');
       });
     } else {
-      alert('Complete todos los campos');
+      this.toastr.error('Por favor verifique los campos', 'Error');
       console.log('Form is not valid');
     }
   }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [LoginComponent],
@@ -12,6 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
     LoginRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   exports: [LoginComponent],
 })
